a019414369ed2c12e2b0aea123189d187e508455
NicoRisso <narisso@uc.cl>
2013-05-02 11:35:51 -0400

    remail fourth try

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   #default from: "from@example.com"
 
 	def receive(email)
-		Rails.logger.debug(emal)
+		Rails.logger.debug(email)
 		p = Pedido.new
 		p.numero = "BORRAME"
 		p.save
