417bec378711e8f63e05479c65b39b981bb86655
NicoRisso <narisso@uc.cl>
2013-05-02 10:19:55 -0400

    remail second try

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,5 @@
 class EmailsController < ApplicationController
-	#skip_before_filter :verify_authenticity_token
+	skip_before_filter :verify_authenticity_token
 
 	def create
 		if request.headers["Authorization"] != "hualpenpedidos1"
