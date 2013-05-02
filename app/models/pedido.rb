0a0696c53375117d15ceb2920bc696092957af32
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-02 16:46:09 -0400

    ...

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,5 +1,4 @@
 class Pedido < ActiveRecord::Base
   establish_connection INTEGRA1_CONF
   attr_accessible :sku, :hora_llegada, :cantidad, :fecha, :rut, :fecha_llegada, :unidad
-  belongs_to :direccion
 end
