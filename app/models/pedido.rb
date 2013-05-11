ca22b81de46a29df9970e5fa936817f9f8023836
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-11 19:41:18 -0400

    ENQUNEUQNEQU

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -2,7 +2,7 @@ class Pedido < ActiveRecord::Base
   establish_connection INTEGRA1_CONF
   attr_accessible :sku, :hora_llegada, :cantidad, :fecha, :rut, :fecha_llegada, :unidad
 
-  def process(id)
+  def self.process(id)
     p = find(id)
     logger.debug "Enqueued: " + p.fecha.to_s + " " + p.hora_llegada.to_s + ": Pedido de " + p.cantidad.to_s + " " + p.unidad.to_s + " de producto sku: " + p.sku.to_s
   end
