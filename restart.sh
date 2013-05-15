6bc0f741d5d66c4331bb2dce672649657ce27668
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-15 16:49:46 -0400

    queeeeueueueueuuuueueee

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
+kill -9 `ps -ef | grep ruby | grep -v grep | awk '{print $2}'`
+
 sudo /etc/init.d/apache2 restart
 
-kill -9 `ps -ef | grep ruby | grep -v grep | awk '{print $2}'`
 nohup bundle exec rake qc:work &
