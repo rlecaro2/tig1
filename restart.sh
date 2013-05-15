c8bdc35f620f4d45b828da1ffba88e00ee7f87a9
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-15 16:45:50 -0400

    ququququququuqueeeee

--- restart.sh
+++ restart.sh
@@ -1,16 +1,4 @@
 sudo /etc/init.d/apache2 restart
 
-within-bundled-project()
-{
-    local dir="$(pwd)"
-    while [ "$(dirname $dir)" != "/" ]; do
-        [ -f "$dir/Gemfile" ] && return
-        dir="$(dirname $dir)"
-    done
-    false
-}
-
-if within-bundled-project;then
-  kill -9 `ps -ef | grep ruby | grep -v grep | awk '{print $2}'`
-  nohup bundle exec rake qc:work &
-fi
+kill -9 `ps -ef | grep ruby | grep -v grep | awk '{print $2}'`
+nohup bundle exec rake qc:work &
