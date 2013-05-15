13bbe5a7c9d553210237a3ae5dcad19764de61b9
Javier Vergara <jevergar@uc.cl>
2013-05-15 17:28:09 -0400

    proceso de pedidos

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -1,13 +1,25 @@
-class Reserva
-
-	attr_accessor :sku
-
-	def self.get(sku)
-		session = GoogleDrive.login("username@gmail.com", "mypassword")
-		ws = session.spreadsheet_by_key("pz7XtlQC-PYx-jrVMJErTcg").worksheets[0]
-	end
-
-	def initialize	
+class Reserva < ActiveRecord::Base
+	establish_connection INTEGRA1_CONF
+	attr_accessible :sku, :cantidad
+	
+	def self.mega_update
+		
+		Reserva.destroy_all
+		session = GoogleDrive.login("hualpenpedidos@gmail.com", "tallerdeintegracion")
+		ws = session.spreadsheet_by_key("0AhRzyWALVmYKdHZSTk1hUmpvR1JjRGFjX0ZZQjhwLVE").worksheets[0]
+		
+		i=2
+		while (true)
+			if (ws[i,1] == "")
+				break
+			end
+			sku=ws[i,1]
+			cantidad=ws[i,2]	
+			r=Reserva.new
+			r.sku=sku
+			r.cantidad=cantidad
+			r.save
+		end
 
 	end
 	
