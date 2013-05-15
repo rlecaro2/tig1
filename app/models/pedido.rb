231d81514a618ba6f33553a8cabe36bad3a00fc1
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-15 18:10:52 -0400

    merge

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -4,51 +4,53 @@ class Pedido < ActiveRecord::Base
 
 
 def self.process (id)
- p=Pedido.find_by_id(id)
- #Se obtiene la direccion
- d=Direccion.find_by_rut(p.rut)
- #Se obtiene la información de contacto
- c=VtigerHelper.getContactByRut(p.rut)
- #Se ve si el cliente es preferencial
- esPreferencial=false
- if c["cf_650"] == "34.242.924-1"
- 	esPreferencial=true
- end
- #Obtencion de las reservas y los stocks
- reservado=Reserva.find_by_sku(p.sku)
- #disponible=Bodegas.obtener_stock_disponible(p.sku,55)
- if esPreferencial
- 	if p.cantidad <= disponible
- 	 reservado.cantidad -= p.cantidad
- 	 if reservado.cantidad <0
- 	 reservado.cantidad=0
- 	 end
- 	 reservado.save
- 	else
- 	 p.status="Quiebre por falta de stock"
- 	 p.save
- 	 return
- 	end
- else
- 	if p.cantidad > disponible-reservado.cantidad
- 	 p.status="Quiebre por falta de stock"
- 	 p.save
- 	 return
- 	end
- end
+  p = Pedido.find_by_id(id)
+  #Se obtiene la direccion
+  d = Direccion.find_by_rut(p.rut)
+  #Se obtiene la información de contacto
+  c = VtigerHelper.getContactByRut(p.rut)
 
- #Bodegas.traspaso(p.sku,p.cantidad,"disponible","despacho")
+  #Se ve si el cliente es preferencial
+  esPreferencial = false
+  if c["cf_650"] == "34.242.924-1" #este es el rut preferencial y cf_650 es el campo que guarda el rut en vTiger
+    esPreferencial = true
+  end
 
- #Acá habría que revisar si el clima permite el despacho
- #Si no se puede despachar hay que reversar la reserva
- #También hay que mover el stock de bodega
- buenClima=true
+  #Obtencion de las reservas y los stocks
+  reservado = Reserva.find_by_sku(p.sku)
+  #disponible = Bodegas.obtener_stock_disponible(p.sku,55)
+  if esPreferencial
+    if p.cantidad <= disponible
+     reservado.cantidad -= p.cantidad
+     if reservado.cantidad < 0
+     reservado.cantidad = 0
+     end
+     reservado.save
+    else
+     p.status = "Quiebre por falta de stock"
+     p.save
+     return
+    end
+  else
+    if p.cantidad > disponible-reservado.cantidad
+     p.status="Quiebre por falta de stock"
+     p.save
+     return
+    end
+  end
 
- if not buenClima
- 	#Bodegas.traspaso(p.sku,p.cantidad,"despacho","disponible")
- else
- 	#Bodegas.despachar(p.sku,p.cantidad)
- end
+  #Bodegas.traspaso(p.sku,p.cantidad,"disponible","despacho")
+
+  #Acá habría que revisar si el clima permite el despacho
+  #Si no se puede despachar hay que reversar la reserva
+  #También hay que mover el stock de bodega
+  buenClima=true
+
+  if not buenClima
+    #Bodegas.traspaso(p.sku,p.cantidad,"despacho","disponible")
+  else
+    #Bodegas.despachar(p.sku,p.cantidad)
+  end
 end
 
 end
--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -8,16 +8,16 @@ class Reserva < ActiveRecord::Base
 		session = GoogleDrive.login("hualpenpedidos@gmail.com", "tallerdeintegracion")
 		ws = session.spreadsheet_by_key("0AhRzyWALVmYKdHZSTk1hUmpvR1JjRGFjX0ZZQjhwLVE").worksheets[0]
 		
-		i=2
+		i = 2
 		while (true)
 			if (ws[i,1] == "")
 				break
 			end
-			sku=ws[i,1]
-			cantidad=ws[i,2]	
-			r=Reserva.new
-			r.sku=sku
-			r.cantidad=cantidad
+			sku = ws[i,1]
+			cantidad = ws[i,2]	
+			r = Reserva.new
+			r.sku = sku
+			r.cantidad = cantidad
 			r.save
 		end
 
