3d0b60c33813b0c9be7c9829e07a5993e2bef19b
Rafa <rafa@ubuntu.ubuntu-domain>
2013-05-15 13:59:22 -0400

    queue6

--- restart.sh
+++ restart.sh
@@ -1,5 +1,23 @@
-pwd
-
 /etc/init.d/apache2 restart
 
-rake qc:work &
+bundler-installed()
+{
+    which bundle > /dev/null 2>&1
+}
+
+within-bundled-project()
+{
+    local dir="$(pwd)"
+    while [ "$(dirname $dir)" != "/" ]; do
+        [ -f "$dir/Gemfile" ] && return
+        dir="$(dirname $dir)"
+    done
+    false
+}
+
+run-with-bundler()
+{
+    if bundler-installed && within-bundled-project; then
+        bundle exec rake qc:work &
+    fi
+}
