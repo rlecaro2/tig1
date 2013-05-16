91d8068459ec25e4249f9a79352b411f07857cbe
NicoRisso <narisso@uc.cl>
2013-05-16 17:24:41 -0400

    rutas

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -11,6 +11,16 @@ class PedidosController < ApplicationController
 
   	@json = @direccion.to_gmaps4rails
 
+    from = "-36.731455,-73.112297"
+    
+    @startLat = '"-36.731455"'
+    @startLng = '"-73.112297"'
+    @endLat = '"'+@direccion.latitude.to_s+'"'
+    @endLng = '"'+@direccion.longitude.to_s+'"'
+
+    to = @direccion.latitude.to_s+','+@direccion.longitude.to_s
+    @dest = Gmaps4rails.destination( {"from"=>from , "to"=>to },options={}, output="pretty")
+
   	respond_to do |format|
   		format.html # showmap.html.erb
   	end
