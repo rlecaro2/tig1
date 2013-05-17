8c94a2c3c21944d6b88b79e2b2a4b0793351fced
NicoRisso <narisso@uc.cl>
2013-05-17 10:45:46 -0400

    logger for queue

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -5,67 +5,76 @@ class Pedido < ActiveRecord::Base
 
   def self.process (id)
 
-    p = Pedido.find_by_id(id)
-    p.status = "Procesando"
-    p.save
-    #Se obtiene la direccion
-    d = p.direccion
-    #Se obtiene la información de contacto
-    c = VtigerHelper.getContactByShipTo(p.direccion_id)
+    begin
 
-    vtiger_order_id = VtigerHelper.createSalesOrder(p.sku, p.rut ,c["cf_641"], p.fecha, p.cantidad, p.unidad)
-
-    #Se ve si el cliente es preferencial
-    esPreferencial = false
-    if c["cf_650"].strip == "34.242.924-1" #este es el rut preferencial y cf_650 es el campo que guarda el rut en vTiger
-      esPreferencial = true
-    end
-
-    #Obtencion de las reservas y los stocks
-    reservado = Reserva.find_by_sku(p.sku)
-    disponible = Bodega.obtener_stock(p.sku,55)
-    if esPreferencial
-      if p.cantidad <= disponible
-       reservado.cantidad -= p.cantidad
-       if reservado.cantidad < 0
-        reservado.cantidad = 0
-       end
-       reservado.save
-      else
-       p.status = "Quiebre por falta de stock"
-       p.save
-       VtigerHelper.cancelSalesOrder(vtiger_order_id)
-       return
-      end
-    else #No es preferencial
-      if p.cantidad > disponible - reservado.cantidad
-       p.status = "Quiebre por falta de stock"
-       p.save
-       VtigerHelper.cancelSalesOrder(vtiger_order_id)
-       return
-      end
-    end
-
-    Bodega.preparar_despacho(p.sku,p.cantidad)
-
-    #Acá habría que revisar si el clima permite el despacho
-    #Si no se puede despachar hay que reversar la reserva
-    #También hay que mover el stock de bodega
-    buenClima = true
-    if not buenClima
-      #Bodegas.traspaso(p.sku,p.cantidad,"despacho","disponible")
-    else
-      Bodega.despachar(p.sku,p.cantidad)
-      p.status = "Despachado"
+      p = Pedido.find_by_id(id)
+      p.status = "Procesando"
       p.save
+      #Se obtiene la direccion
+      d = p.direccion
+      #Se obtiene la información de contacto
+      c = VtigerHelper.getContactByShipTo(p.direccion_id)
 
-      t = Transaccion.new
-      t.pedido = p
-      t.save
+      vtiger_order_id = VtigerHelper.createSalesOrder(p.sku, p.rut ,c["cf_641"], p.fecha, p.cantidad, p.unidad)
 
-      VtigerHelper.dispatchSalesOrder(vtiger_order_id)
+      #Se ve si el cliente es preferencial
+      esPreferencial = false
+      if c["cf_650"].strip == "34.242.924-1" #este es el rut preferencial y cf_650 es el campo que guarda el rut en vTiger
+        esPreferencial = true
+      end
 
+      #Obtencion de las reservas y los stocks
+      reservado = Reserva.find_by_sku(p.sku)
+      disponible = Bodega.obtener_stock(p.sku,55)
+      if esPreferencial
+        if p.cantidad <= disponible
+         reservado.cantidad -= p.cantidad
+         if reservado.cantidad < 0
+          reservado.cantidad = 0
+         end
+         reservado.save
+        else
+         p.status = "Quiebre por falta de stock"
+         p.save
+         VtigerHelper.cancelSalesOrder(vtiger_order_id)
+         return
+        end
+      else #No es preferencial
+        if p.cantidad > disponible - reservado.cantidad
+         p.status = "Quiebre por falta de stock"
+         p.save
+         VtigerHelper.cancelSalesOrder(vtiger_order_id)
+         return
+        end
+      end
+
+      Bodega.preparar_despacho(p.sku,p.cantidad)
+
+      #Acá habría que revisar si el clima permite el despacho
+      #Si no se puede despachar hay que reversar la reserva
+      #También hay que mover el stock de bodega
+      buenClima = true
+      if not buenClima
+        #Bodegas.traspaso(p.sku,p.cantidad,"despacho","disponible")
+      else
+        Bodega.despachar(p.sku,p.cantidad)
+        p.status = "Despachado"
+        p.save
+
+        t = Transaccion.new
+        t.pedido = p
+        t.save
+
+        VtigerHelper.dispatchSalesOrder(vtiger_order_id)
+      end 
+
+      QUEUE_LOGGER.info('Successfully processed pedido ' +id.to_s )
+
+    rescue Exception => ex
+      QUEUE_LOGGER.info(ex.message)
+      QUEUE_LOGGER.info(ex.backtrace.join("\n"))
     end
+
   end
 
 
