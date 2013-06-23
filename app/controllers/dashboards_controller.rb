59b29c2265e261e721393a58d288edf36fb89a06
Rafa <rafa@ubuntu.ubuntu-domain>
2013-06-23 19:04:40 -0400

    Dashhhh

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,8 @@
 class DashboardsController < ApplicationController
 
   def index
-    reportes = Reporte.all.to_a
+    reportes = Reporte.all.asc(:fecha).to_a
     self.ProcessInfo(reportes)
-
-
 
     respond_to do |format|
       format.html # showmap.html.erb
@@ -16,10 +14,8 @@ class DashboardsController < ApplicationController
     @despachos_quiebres = []
     reportes.each do |reporte|
       @ingresos_costos << [reporte.fecha.to_s, reporte.ingresos.to_s, reporte.costos.to_s]
-
       @despachos_quiebres << [reporte.fecha.to_s, reporte.despachos.to_s, reporte.quiebres.to_s]
     end
-
   end
 
 end
